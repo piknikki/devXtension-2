4a9a0906294e99d5b4051afda0e83f607fee530d
Nikki Petersen <piknik@me.com>
2019-07-03 09:57:29 -0600

    added route for getting profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,12 +2,31 @@
 
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-// @route     GET api/profiles ---
-// @desc      Test route ---
-// @access    Public  --- no token
-router.get('/', (req, res) => {
-    res.send('Profile route')
+// @route     GET api/profile/me --- my profile
+// @desc      get current user profile
+// @access    Private --- need middleware, add as second param into route
+router.get('/me', auth, async (req, res) => {
+    // res.send('Profile route') --- test route
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+
+        if(!profile) {
+            return res.status(400).json({ msg: 'There is no profile for this user' })
+        }
+
+        // if there is a profile, send as a json
+        res.json(profile);
+
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('server error')
+    }
+
 });
 
 module.exports = router;
