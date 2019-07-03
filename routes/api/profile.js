532cfaad0b28d7fd16722483dce563973894c38e
Nikki Petersen <piknik@me.com>
2019-07-03 10:54:42 -0600

    added routes for create and update profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
@@ -26,7 +27,90 @@ router.get('/me', auth, async (req, res) => {
         console.error(err.message);
         res.status(500).send('server error')
     }
-
 });
+
+
+// @route     POST api/profile/
+// @desc      create or update user profile
+// @access    Private --- need middleware, add as second param into route
+router.post('/',
+    [auth, [
+        check('status', 'Status is required')
+            .not()
+            .isEmpty(),
+        check('skills', 'Skills is required')
+            .not()
+            .isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        // destructured
+        const {
+            company,
+            website,
+            location,
+            bio,
+            status,
+            githubusername,
+            skills,
+            youtube,
+            facebook,
+            twitter,
+            instagram,
+            linkedin
+        } = req.body;
+
+        // build profile object
+        const profileFields = {};
+        profileFields.user = req.user.id; // gets this from the token that was sent
+        if (company) profileFields.company = company;
+        if (website) profileFields.website = website;
+        if (location) profileFields.location = location;
+        if (bio) profileFields.bio = bio;
+        if (status) profileFields.status = status;
+        if (githubusername) profileFields.githubusername = githubusername;
+        if (skills) {profileFields.skills = skills.split(', ').map(skill => skill.trim())
+        };
+
+        // build social object
+        profileFields.social = {}
+        if (youtube) profileFields.social.youtube = youtube;
+        if (facebook) profileFields.social.facebook = facebook;
+        if (twitter) profileFields.social.twitter = twitter;
+        if (instagram) profileFields.social.instagram = instagram;
+        if (linkedin) profileFields.social.linkedin = linkedin;
+
+        try {
+            let profile = await Profile.findOne({ user: req.user.id }); // the id comes in from the token
+
+            if (profile) {
+                //update -- set profile fields as new input to save, tell it that it's new
+                profile = await Profile.findOneAndUpdate(
+                    { user: req.user.id },
+                    { $set: profileFields },
+                    { new: true }
+                    );
+
+                //  return the whole profile
+                return res.json(profile);
+            }
+
+            //  Create a profile if not already created (which would be caught in the block above)
+            profile = new Profile(profileFields);
+
+            await profile.save(); // save into the db
+            res.json(profile); // send back the profile as a json
+
+        } catch (err) {
+            console.log(err.message);
+            res.status(500).send('server error')
+        }
+
+ });
 
 module.exports = router;
