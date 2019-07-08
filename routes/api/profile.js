4764e6b8666701be9a0a332d38126b87de8f70d6
Nikki Petersen <piknik@me.com>
2019-07-08 09:09:06 -0600

    added github Oauth and route to get 5 repos into profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,8 @@
 // for profiles
 
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
@@ -259,9 +261,6 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
 });
 
 
-
-
-
 // @route     PUT api/profile/education
 // @desc      add education to the profile
 // @access    Private
@@ -345,6 +344,36 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
         await profile.save();
         res.json(profile);
 
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('server error')
+
+    }
+});
+
+// @route     GET api/profile/github/:username
+// @desc      get user repos from GitHub
+// @access    Public
+router.get('/github/:username', async (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&
+            sort=created:asc&
+            client_id=${config.get('githubClientId')}&
+            client_secret=${config.get("githubClientSecret")}`,
+            method: 'GET',
+            headers: { 'user-agent': 'node.js' }
+        }
+
+        request(options, (error, response, body) => {
+            if (error) console.error(error);
+
+            if (response.statusCode !== 200) {
+                return res.status(404).json({ msg: 'No GitHub profile found' })
+            }
+
+            res.json(JSON.parse(body));
+        })
     } catch (err) {
         console.error(err.message);
         res.status(500).send('server error')
