1061bde198d438892ca94d2af970443cacb588ba
Nikki Petersen <piknik@me.com>
2019-07-09 08:19:48 -0600

    added routes for front end

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className="navbar bg-dark">
+        <nav className='navbar bg-dark'>
             <h1>
-                <a href="index.html"><i className="fas fa-code"></i> Dev X-tension</a>
+                <Link to='index.html'><i className="fas fa-code"></i> Dev X-tension</Link>
             </h1>
             <ul>
-                <li><a href="profiles.html">Developers</a></li>
-                <li><a href="register.html">Register</a></li>
-                <li><a href="login.html">Login</a></li>
+                <li><Link to='!#'>Developers</Link></li>
+                <li><Link to='/register'>Register</Link></li>
+                <li><Link to='/login'>Login</Link></li>
             </ul>
         </nav>
     )
