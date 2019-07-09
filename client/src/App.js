8c8983b0f05f04c96b48028aba3a3f38fcf11aa9
Nikki Petersen <piknik@me.com>
2019-07-09 10:12:05 -0600

    added store, provider, reducer for redux

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,30 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+
+// Redux imports
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const  App = () => {
     return (
-        <Router>
-          <Fragment>
-            <Navbar />
-              <Route exact path='/' component={Landing} />
-              <section className="container">
-                <Switch>
-                  <Route exact path='/register' component={Register} />
-                  <Route exact path='/login' component={Login} />
-                </Switch>
-              </section>
+        <Provider store={store}>
+          <Router>
+            <Fragment>
+              <Navbar />
+                <Route exact path='/' component={Landing} />
+                <section className="container">
+                  <Switch>
+                    <Route exact path='/register' component={Register} />
+                    <Route exact path='/login' component={Login} />
+                  </Switch>
+                </section>
 
-          </Fragment>
-        </Router>
+            </Fragment>
+          </Router>
+        </Provider>
 )};
 
 
