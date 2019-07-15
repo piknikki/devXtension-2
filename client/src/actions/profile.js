071f3c04f8fd01f6b2a22a14141af6fe3f0e23e1
Nikki Petersen <piknik@me.com>
2019-07-15 12:11:07 -0600

    added ability to delete ed, exp, and account

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import { setAlert } from "./alert";
 
 import {
+    ACCOUNT_DELETED,
+    CLEAR_PROFILE,
     GET_PROFILE,
     PROFILE_ERROR,
     UPDATE_PROFILE
@@ -152,3 +154,86 @@ export const addEducation = (formData, history) => async dispatch => {
     }
 };
 
+// delete experience -- hit ap/profile/experience/:exp_id route
+export const deleteExperience = id => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/profile/experience/${id}`);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Experience removed', 'success'));
+    } catch (err) {
+        const errors = err.response.data.errors;
+
+        // if there are errors, for every error, send the message in the danger css style
+        // this references the setAlert in the alert.js reducer file
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// delete education -- hit ap/profile/education/:edu_id route
+export const deleteEducation = id => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/profile/education/${id}`);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Education removed', 'success'));
+    } catch (err) {
+        const errors = err.response.data.errors;
+
+        // if there are errors, for every error, send the message in the danger css style
+        // this references the setAlert in the alert.js reducer file
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// delete ACCOUNT -- hit ap/profile route
+export const deleteAccount = () => async dispatch => {
+
+    if (window.confirm("Are you sure?  This can NOT be undone.")) {
+        try {
+            const res = await axios.delete('/api/profile');
+
+            dispatch({ type: CLEAR_PROFILE });
+            dispatch({ type: ACCOUNT_DELETED });
+
+            dispatch(setAlert('Your account has been permanently deleted.'));
+        } catch (err) {
+            const errors = err.response.data.errors;
+
+            // if there are errors, for every error, send the message in the danger css style
+            // this references the setAlert in the alert.js reducer file
+            if (errors) {
+                errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+            }
+
+            dispatch({
+                type: PROFILE_ERROR,
+                payload: { msg: err.response.statusText, status: err.response.status }
+            });
+        }
+
+    }
+
+};
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -8,6 +8,7 @@ const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route     GET api/profile/me
 // @desc      get current user profile
@@ -158,7 +159,7 @@ router.get("/user/:user_id", async (req, res) => {
 router.delete("/", auth, async (req, res) => {
     try {
         // remove posts
-
+        await Post.deleteMany({ user: req.user.id });
         // remove profile
         await Profile.findOneAndRemove({ user: req.user.id });
         // remove user
