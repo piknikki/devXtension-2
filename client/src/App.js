7655024ee34a6f7b3084efb8dc432c5337b2587d
Nikki Petersen <piknik@me.com>
2019-07-15 12:59:16 -0600

    added ability to display profiles

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
+import Profiles from './components/profiles/Profiles';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 
@@ -45,11 +46,13 @@ const  App = () => {
                   <Switch>
                     <Route exact path='/register' component={Register} />
                     <Route exact path='/login' component={Login} />
+                    <Route exact path='/profiles' component={Profiles} />
                     <PrivateRoute exact path='/dashboard' component={Dashboard} />
                     <PrivateRoute exact path='/create-profile' component={CreateProfile} />
                     <PrivateRoute exact path='/edit-profile' component={EditProfile} />
                     <PrivateRoute exact path='/add-experience' component={AddExperience} />
                     <PrivateRoute exact path='/add-education' component={AddEducation} />
+
                   </Switch>
                 </section>
 
