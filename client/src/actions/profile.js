96b47ef9ef2e28a19652e08b993c81a5da5cced0
Nikki Petersen <piknik@me.com>
2019-07-15 12:28:42 -0600

    added github repo stuff

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -2,11 +2,13 @@ import axios from 'axios';
 import { setAlert } from "./alert";
 
 import {
-    ACCOUNT_DELETED,
-    CLEAR_PROFILE,
     GET_PROFILE,
+    GET_PROFILES,
     PROFILE_ERROR,
-    UPDATE_PROFILE
+    UPDATE_PROFILE,
+    CLEAR_PROFILE,
+    ACCOUNT_DELETED,
+    GET_REPOS
 } from "./types";
 
 //  Get current users profile
@@ -18,6 +20,66 @@ export const getCurrentProfile = () => async dispatch => {
         // token has user id
         dispatch({
             type: GET_PROFILE,
+            payload: res.data
+        });
+
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+};
+
+// get all profiles
+export const getProfiles = () => async dispatch => {
+    dispatch({ type: CLEAR_PROFILE });
+
+    try {
+        const res = await axios.get('/api/profile');
+
+        // token has user id
+        dispatch({
+            type: GET_PROFILES,
+            payload: res.data
+        });
+
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+};
+
+// get profile by ID
+export const getProfileById = userId => async dispatch => {
+
+    try {
+        const res = await axios.get(`/api/profile/user/${userId}`);
+
+        // token has user id
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+};
+
+// Get GitHub repos
+export const getGithubRepos = username => async dispatch => {
+    try {
+        const res = await axios.get(`/api/profile/github/${username}`);
+
+        // token has user id
+        dispatch({
+            type: GET_REPOS,
             payload: res.data
         });
 
