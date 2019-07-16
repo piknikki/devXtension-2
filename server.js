7ce25677108bf2cae9505b2ed33c11305230e773
Nikki Petersen <piknik@me.com>
2019-07-16 15:41:18 -0600

    trying to deploy to heroku

--- server.js
+++ server.js
@@ -22,11 +22,14 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
+    // Set static folder
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', index.html))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
-app.listen(process.env.PORT || 5000);  // listen to port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
