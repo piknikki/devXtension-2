164331358d82b9d9cc917f55face7e6731b69222
Nikki Petersen <piknik@me.com>
2019-07-16 14:58:21 -0600

    deploy to heroku

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express'); // import express
 // import db connection from db.js
 const connectDB = require('./config/db');
 
+const path = require('path');
+
 const app = express();  // use express as app
 
 // connect database from above
@@ -12,15 +14,20 @@ connectDB();
 app.use(express.json({ extended: false }))
 
 
-app.get("/", (req, res) => {
-    res.send("API running"); // puts the message in the window
-});
-
 // define routes, to use routes in ./routes/api
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', index.html))
+    });
+}
 
 const PORT = process.env.PORT || 5000;  // find port in the env file as PORT or use 5000
 
